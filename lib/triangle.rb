d5993526ddbd53afe807643f58d7cba9dac17834
Tyler Croley <tjcroley@outlook.com>
2020-03-18 22:26:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,42 +1,15 @@
 class Triangle
-  # write code here\
-
-  def initialize(s1, s2, s3)
-
+  attr_accessor :x, :y, :z 
   
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
-
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  def initialize()
+    @x = x 
+    @y = y 
+    @z = z 
   end
-
-  def kind
-    if @s1 == @s2 && @s1 == @s3
-      return :equilateral
-    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
-      return :isosceles
-    else
-      return :scalene
+  
+  def kind 
+    if x == y && y == z && z = x 
+      :equilateral
     end
-  end
-
-end
-
-
-
-class TriangleError < StandardError
-  def message
-    "This is not a valid triangle."
   end
 end
