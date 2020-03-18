622390f8fb157f619be2d834f9ea457a7e5ac311
Tyler Croley <tjcroley@outlook.com>
2020-03-18 22:36:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
   def kind 
     if @s1 == 0 && @s2 == 0 && @s3 = 0 
       TriangleError
+    end
     if @s1 == @s2 && @s2 == @s3
      :equilateral
    elsif @s1 != @s2 && @s2 != @s3 && @s3 != @s1
