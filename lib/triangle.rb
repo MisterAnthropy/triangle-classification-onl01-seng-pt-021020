b8928255a5b3ea895b16cb33aaf37be3c321a14e
Tyler Croley <tjcroley@outlook.com>
2020-03-18 22:38:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,15 +2,15 @@ class Triangle
   attr_accessor :s1, :s2, :s3 
   
   def initialize(s1, s2, s3)
+     if @s1 == 0 && @s2 == 0 && @s3 = 0 
+      TriangleError
+    end
     @s1 = s1 
     @s2 = s2
     @s3 = s3 
   end
   
   def kind 
-    if @s1 == 0 && @s2 == 0 && @s3 = 0 
-      TriangleError
-    end
     if @s1 == @s2 && @s2 == @s3
      :equilateral
    elsif @s1 != @s2 && @s2 != @s3 && @s3 != @s1
